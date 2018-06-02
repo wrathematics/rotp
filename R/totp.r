8d92ebb23c3fbebdd8a5d01a6c0a25ac359e4db8
Drew Schmidt <wrathematics@gmail.com>
2018-06-01 22:41:10 -0400

    add authenticator interface

---
+++ R/totp.r
@@ -0,0 +1,57 @@
+get_time_rem = function(interval=30L)
+{
+  interval - (as.integer(Sys.time()) %% interval)
+}
+
+
+
+totp = function(key)
+{
+  .Call(R_totp, key)
+}
+
+
+
+progress_bar = function(rem, chars=30L, interval=30L)
+{
+  n_empty = as.integer(rem/interval*chars)
+  n_full = chars - n_empty
+  
+  paste0(
+    "[",
+    paste0(rep("=", n_full), collapse=""),
+    paste0(rep("-", n_empty), collapse=""),
+    "]",
+    collapse=""
+  )
+}
+
+
+auth = function(name)
+{
+  check.is.string(name)
+  
+  key = decrypt(db_getkey(name)$encrypted_key)
+  cat("Ctrl+c to exit\n")
+  
+  while (TRUE)
+  {
+    rem = get_time_rem()
+    p = totp(key)
+    p_str = sprintf("%06d", p)
+    
+    while (rem > 0)
+    {
+      # cat('\r', paste0(p_str, " (", rem, " seconds remaining) "))
+      cat('\r', paste0(p_str, " (", sprintf("%2d", rem), " seconds remaining ", progress_bar(rem), ") "))
+      flush.console()
+      rem = rem - 1L
+      Sys.sleep(1)
+    }
+  }
+  
+  rm(key)
+  invisible(gc(verbose=FALSE, reset=TRUE))
+  
+  invisible(NULL)
+}
---
+++ R/totp_repl.r
@@ -0,0 +1,43 @@
+#' runauth
+#' 
+#' Interactive authenticator interface.
+#' 
+#' @export
+runauth = function()
+{
+  check.is.interactive()
+  
+  prompt = "$ "
+  
+  if (db_len() == 0)
+    stop("No keys stored in db! Add some by first running rundb()\n")
+  else
+    names = db_list()
+  
+  choices_verbose = names
+  choices = 1:length(choices_verbose)
+  
+  flush.console()
+  cat("Pick a key or enter Q/q to exit:\n")
+  cat(" ", paste(choices, choices_verbose, sep=" - ", collapse="\n  "), "\n")
+  
+  choice = getPass::getPass(prompt)
+  
+  while (choice != "Q" && choice != "q" && all(choice != choices))
+  {
+    cat("ERROR: please choose one of", paste(choices, collapse=", "), "\n")
+    choice = readline(prompt)
+  }
+  
+  if (choice == "Q" || choice == "q")
+    return(invisible())
+  
+  choice = as.integer(choice)
+  
+  cat("\n")
+  
+  if (choice < length(choices))
+    auth(choices_verbose[choice])
+  
+  invisible(NULL)
+}
