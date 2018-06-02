ca16f69549806806a3dbae154d7627cc558a6d9d
Drew Schmidt <wrathematics@gmail.com>
2018-06-01 23:00:54 -0400

    register native routines

---
+++ src/rotp_native.c
@@ -0,0 +1,19 @@
+/* Automatically generated. Do not edit by hand. */
+
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include <stdlib.h>
+
+extern SEXP R_totp(SEXP key_);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"R_totp", (DL_FUNC) &R_totp, 1},
+  {NULL, NULL, 0}
+};
+
+void R_init_rotp(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
