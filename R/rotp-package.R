da20417ded3699f8ef5dc95419bd7ead87defa8a
Drew Schmidt <wrathematics@gmail.com>
2018-06-01 22:42:45 -0400

    add package help file

---
+++ R/rotp-package.R
@@ -0,0 +1,11 @@
+#' rotp
+#' 
+#' TODO
+#' 
+#' @useDynLib rotp R_totp
+#' 
+#' @name rotp-package
+#' @docType package
+#' @author Drew Schmidt \email{wrathematics AT gmail.com}
+#' @keywords Package
+NULL
