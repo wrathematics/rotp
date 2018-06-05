fa8839e113e4db80b174ceb6985e3c9545ebf796
Drew Schmidt <wrathematics@gmail.com>
2018-06-04 22:40:34 -0400

    add hotp test

---
+++ tests/hotp.r
@@ -0,0 +1,18 @@
+library(rotp)
+key = "asdf"
+
+test = hotp(key, 1)
+truth = 679366
+stopifnot(all.equal(test, truth))
+
+test = hotp(key, -1)
+truth = 602401
+stopifnot(all.equal(test, truth))
+
+test = hotp(key, 10000000)
+truth = 620310
+stopifnot(all.equal(test, truth))
+
+test = hotp(key, -10000000)
+truth = 229514
+stopifnot(all.equal(test, truth))
