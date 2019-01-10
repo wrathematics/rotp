3774defb52a8798c59d8d3751ee4fa9a5030c784
wrathematics <wrathematics@gmail.com>
2019-01-10 18:26:57 -0500

    update tests

--- tests/hotp.r
+++ tests/hotp.r
@@ -5,16 +5,16 @@ test = hotp(key, 1)
 truth = 679366
 stopifnot(all.equal(test, truth))
 
-test = hotp(key, -1)
-truth = 602401
+test = hotp(key, 10)
+truth = 795860
+stopifnot(all.equal(test, truth))
+
+test = hotp(key, 10000)
+truth = 350230
 stopifnot(all.equal(test, truth))
 
 test = hotp(key, 10000000)
 truth = 620310
-stopifnot(all.equal(test, truth))
-
-test = hotp(key, -10000000)
-truth = 229514
 stopifnot(all.equal(test, truth))
 
 
